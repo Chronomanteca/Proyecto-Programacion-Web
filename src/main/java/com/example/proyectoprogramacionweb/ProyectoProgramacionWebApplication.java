24836a7b1e502b0c482a691fb47c07f9cd488da0
Daniel Morales <danisan98@hotmail.es>
2021-09-17 14:02:40 -0500

    Initial Commit

---
+++ src/main/java/com/example/proyectoprogramacionweb/ProyectoProgramacionWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.proyectoprogramacionweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoProgramacionWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoProgramacionWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/proyectoprogramacionweb/ProyectoProgramacionWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proyectoprogramacionweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoProgramacionWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
