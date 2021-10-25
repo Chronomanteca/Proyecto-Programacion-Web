44b9e799145380ea748e68cef2d28c2c9c0cd506
Daniel Morales <danisan98@hotmail.es>
2021-10-25 01:03:43 -0500

    Validacion de fecha agregada

--- src/main/java/com/example/proyectoprogramacionweb/Users/Appointment/Domain/ValueObjects/AppointmentDate.java
+++ src/main/java/com/example/proyectoprogramacionweb/Users/Appointment/Domain/ValueObjects/AppointmentDate.java
@@ -1,6 +1,17 @@
 package com.example.proyectoprogramacionweb.Users.Appointment.Domain.ValueObjects;
 
 import com.example.proyectoprogramacionweb.Shared.Domain.Aggregate.StringValueObject;
+import com.example.proyectoprogramacionweb.Users.Appointment.Domain.Exceptions.InvalidAppointmentDate;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import org.apache.tomcat.jni.Local;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.Locale;
 
 public class AppointmentDate extends StringValueObject {
     private AppointmentDate(){}
@@ -10,8 +21,19 @@ public class AppointmentDate extends StringValueObject {
     }
 
     private void validate(String date) {
-        //TODO: Validacion de la fecha
-    }
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime test = LocalDateTime.parse(date,dtf);
+        if(test.isBefore(now)){
+            throw new InvalidAppointmentDate("No se puede crear una cita antes de la fecha actual\nFecha de hoy: "+dtf.format(now)+" Fecha ingresada "+dtf.format(test));
+        }
+
+
+        }
+
+
+
+
 
     private void CurrentDate(String date){
 
